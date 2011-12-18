fecf12ef7ffbe8b15dd6c760c268d6374da287bb
Robert Levy <r.p.levy@gmail.com>
2011-12-17 23:56:45 -0500

    upgrade library dependencies and other minor cleanup

--- src/com/twinql/clojure/core.clj
+++ src/com/twinql/clojure/core.clj
@@ -1,5 +1,5 @@
 (ns com.twinql.clojure.core
   (require
    [com.twinql.clojure
-    NaiveTrustManager PermissiveHostnameVerifier 	async_client async_libs
+    NaiveTrustManager PermissiveHostnameVerifier async_client async_libs
     http sync_libs x509_connection_manager]))
